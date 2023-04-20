1a53ce2cc63ccecdc58f8eace266ab752d3f7f8f
krzysztof.obrebski <Krzysztof.Obrebski@GAINCapital.com>
2023-04-20 19:41:09 +0200

    change directory layout, get version from git tag

--- conanfile.py
+++ conanfile.py
@@ -2,9 +2,16 @@ import os
  
 from conans import ConanFile, CMake, tools
 
+def get_verion_tag():
+    git = tools.Git("stonex-cms-amqp-test-cases")
+    try:
+        return "%s" % (git.get_tag())
+    except Exception as e:
+        return e
+        
 class StonexCMSAMQPTestCases(ConanFile):
     name = "stonex-cms-amqp-test-cases"
-    version = "0.0.1"
+    version = get_verion_tag()
     license = "<Put the package license here>"
     author = "krzysztof.obrebski@stonex.com"
     url = "<Package recipe repository url here, for issues about the package>"
@@ -30,7 +37,7 @@ class StonexCMSAMQPTestCases(ConanFile):
 
 
     def package(self):
-        self.copy("*.h", dst="include",src="include")
+        self.copy("*.h", dst="include",src="CoreTests\include")
         self.copy("*.lib", dst="lib",src="lib", keep_path=False)
         self.copy("*.pdb", dst="lib",src="lib", keep_path=False)
 
